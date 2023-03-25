db41c3a21bf6b2a6c0887122a095325468ef4206
nuci <kirianuci@gmail.com>
2023-03-25 21:20:56 +0400

    Fixed file structure

--- datesend/src/App.js
+++ datesend/src/App.js
@@ -1,5 +1,5 @@
 
-import Home from "./Home";
+import Home from "./Page/Home.jsx";
 
 function App() {
 
--- datesend/src/Page/Home.jsx
+++ datesend/src/Page/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import "../Style/Home.css"
 const Home = () => {
  const [name, setName] = useState("");
  const [email, setEmail] = useState("");
